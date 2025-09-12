79753b71e46424a510c6d70a1e60f79eabfa8665
Amirhossein <ahr.developing@gmail.com>
2025-09-12 12:00:24 +0330

    Add Navbar & run the infinite moving cards ( testimonials )

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { NavbarDemo } from "@/components/Navbar";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNav";
 
 export default function Home() {
   return (
     <main className="relative bg-slate-950 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
+        <NavbarDemo />
         {/* <FloatingNav navItems={} /> */}
         <Hero />
         <Grid />
